ca6795c5dab2d0ffcc4d4e2b24069a406a1bb36e
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-06-09 18:36:21 +0200

    style entrance, navigate + button start

--- src/pages/Entrance/Entrance.jsx
+++ src/pages/Entrance/Entrance.jsx
@@ -1,11 +1,12 @@
 import "./Entrance.css";
-import information from "./imgs/information_small.png"
-import translate from "./imgs/translate_small.png"
-import gameTitle from "./imgs/logo_new.png"
+import information from "./imgs/rules_small.png"
+import gameTitle from "./imgs/logowhite_small.png"
 import poop from "./imgs/poop_small.png"
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Entrance = () => {
+  const navigate = useNavigate()
     return (
     <main className="start-screen">
       <div className="start-screen-content">
@@ -13,20 +14,15 @@ export const Entrance = () => {
       <Link to="/rules"><img src= {information} alt="Info icon" className="start-screen__info-icon"/></Link>
     </button>
 
-    <button className="start-screen__language-btn" aria-label="Change language">
-      <img src={translate} alt="Language icon" className="start-screen__language-icon"/>
-    </button>
-
     <h1 className="start-screen__title visually-hidden">Poop Patrol</h1>
     <Link to="/gamePage"><img  src={gameTitle} alt="Poop Patrol logo" className="start-screen__title-img"/></Link>
 
     <Link to="/gamePage"><div className="start-screen__dog"></div></Link>
 
-    <button className="start-screen__poop-btn" aria-label="Start game">
-      <Link to="/gamePage"><img src={poop} alt="Poop icon" className="start-screen__poop-icon"/></Link>
+    <button className="start-screen__btn" onClick={() => {navigate("/gamePage")}} aria-label="Start">
+      START GAME<img src={poop} alt="Poop icon" className="start-screen__poop-icon"/>
     </button>
 
-    <Link to="gamePage/"><p className="start-screen__text">Pick me up to continue</p></Link>
     </div>
     </main>
     )
