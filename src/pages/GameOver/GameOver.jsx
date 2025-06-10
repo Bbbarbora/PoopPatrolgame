4f7006d72bcd4701730b165fbef7a6a690f12859
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-06-10 10:00:18 +0200

    gameOver screen update pictures

--- src/pages/GameOver/GameOver.jsx
+++ src/pages/GameOver/GameOver.jsx
@@ -1,24 +1,26 @@
 import "./GameOver.css";
 import { useLocation } from "react-router-dom";
-import poop from "./imgs/poop_small.png"
-import educative from "./imgs/educative_small.png"
+import poop from "./imgs/poop_small.png";
+import educative from "./imgs/educative_small.png";
 import { useNavigate } from "react-router-dom";
 
-
 export const GameOver = () => {
-  const location = useLocation()
-  const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div className="gameover-screen">
       <h2 className="gameover-screen__title">Game Over</h2>
-      
+
       <div className="gameover-screen__educative">
-        <img src= {educative} alt="educative" />
+        <img src={educative} alt="educative" />
       </div>
-      
+
       <p className="gameover-screen__score">
-        Your score: <span id="final-score">{location.state.score}
-          <img src= {poop} alt="poop" />
+        {/* Your score:{" "} */}
+        <span className="label">Your score:</span>
+        <span id="final-score">
+          {location.state.score}
+          <img src={poop} alt="poop" className="final-score__icon" />
         </span>
       </p>
 
@@ -27,8 +29,14 @@ export const GameOver = () => {
         park!
       </p>
 
-      <button className="gameover-screen__btn" onClick={() => {navigate("/gamePage")}} aria-label="Play again">
-        Play again 
+      <button
+        className="gameover-screen__btn"
+        onClick={() => {
+          navigate("/gamePage");
+        }}
+        aria-label="Play again"
+      >
+        Play again
       </button>
     </div>
   );
